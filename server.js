39b71ea1bcf57f73727453aa199be4a927da0c02
Glitch (fcc-filemeta) <none>
2018-05-20 20:17:16 +0000

    💈💘 Checkpoint ./server.js:1015202/226 ./views/index.html:1015202/2 ./uploads/readme.js:1015202/2

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 'use strict';
 
-var express = require('express');
-var cors = require('cors');
-
+const express = require('express');
+const cors = require('cors');
 // require and use "multer"...
+const multer = require('multer');
+const upload = multer({ dest: "/uploads/" });
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -16,6 +17,10 @@ app.get('/', function (req, res) {
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
+});
+
+app.post("/api/analyze", () => {
+  
 });
 
 app.listen(process.env.PORT || 3000, function () {
